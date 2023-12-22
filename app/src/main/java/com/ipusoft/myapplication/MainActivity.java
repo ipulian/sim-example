fd420617edba8a558aa3c4238e05702f0ca719c3
ipusdk <team.github@51lianlian.com>
2023-12-22 15:58:32 +0800

    bugfix

--- app/src/main/java/com/ipusoft/myapplication/MainActivity.java
+++ app/src/main/java/com/ipusoft/myapplication/MainActivity.java
@@ -8,10 +8,8 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.CallLog;
 import android.provider.Settings;
 import android.util.Log;
-import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,59 +17,47 @@ import android.widget.EditText;
 import com.elvishew.xlog.XLog;
 import com.ipusoft.context.AppContext;
 import com.ipusoft.context.IpuSoftSDK;
-import com.ipusoft.context.base.IObserver;
 import com.ipusoft.context.bean.AuthInfo;
 import com.ipusoft.context.bean.SimRiskControlBean;
 import com.ipusoft.context.bean.SysRecording;
 import com.ipusoft.context.component.ToastUtils;
 import com.ipusoft.context.config.Env;
-import com.ipusoft.context.constant.DateTimePattern;
 import com.ipusoft.context.manager.PhoneManager;
 import com.ipusoft.localcall.UploadFileObserve;
 import com.ipusoft.localcall.bean.SysCallLog;
 import com.ipusoft.localcall.bean.UploadResponse;
-import com.ipusoft.localcall.bean.UploadSysRecordingBean;
 import com.ipusoft.localcall.constant.UploadStatus;
-import com.ipusoft.localcall.datastore.SimDataRepo;
-import com.ipusoft.localcall.module.UploadService;
+import com.ipusoft.localcall.manager.UploadManager;
 import com.ipusoft.localcall.repository.CallLogRepo;
-import com.ipusoft.localcall.repository.FileRepository;
 import com.ipusoft.localcall.repository.RecordingFileRepo;
-import com.ipusoft.oss.AliYunManager;
+import com.ipusoft.mmkv.datastore.CommonDataRepo;
 import com.ipusoft.oss.AliYunUploadManager;
+import com.ipusoft.permission.RxPermissionUtils;
+import com.ipusoft.sim.RecordingDataRepo;
 import com.ipusoft.sim.http.SimHttp;
 import com.ipusoft.sim.iface.OnSimCallPhoneResultListener;
 import com.ipusoft.utils.AppUtils;
 import com.ipusoft.utils.ArrayUtils;
-import com.ipusoft.utils.DateTimeUtils;
-import com.ipusoft.utils.EncodeUtils;
 import com.ipusoft.utils.ExceptionUtils;
-import com.ipusoft.utils.FileUtilsKt;
 import com.ipusoft.utils.GsonUtils;
 import com.ipusoft.utils.StringUtils;
-import com.ipusoft.utils.ThreadUtils;
+import com.ipusoft.utils.SysRecordingUtils;
 import com.tbruyelle.rxpermissions3.RxPermissions;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.annotations.NonNull;
-import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.ObservableOnSubscribe;
-import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "MainActivity";
 
-    private Button btnDial, btnQueryCallLog, btnQueryRecording, btnUpload, btnStorageManagerPermission;
+    private Button btnDial, btnQueryCallLog, btnQueryRecording, btnUpload, btnUpload2, btnQueryList, btnQueryCount, btnStorageManagerPermission;
 
     private List<SysCallLog> callLogList;
     private List<File> fileList;
+
+    private SysRecording sysRecording;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,6 +71,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnQueryCallLog = (Button) findViewById(R.id.btn_query_calllog);
         btnQueryRecording = (Button) findViewById(R.id.btn_query_recording);
         btnUpload = (Button) findViewById(R.id.btn_upload);
+        btnUpload2 = (Button) findViewById(R.id.btn_upload2);
+        btnQueryCount = (Button) findViewById(R.id.btn_queryCount);
+        btnQueryList = (Button) findViewById(R.id.btn_queryList);
         btnStorageManagerPermission = (Button) findViewById(R.id.btn_storage_manager_permission);
 
         btnDial.setOnClickListener(this);
@@ -92,6 +81,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnQueryRecording.setOnClickListener(this);
         btnDial.setOnClickListener(this);
         btnUpload.setOnClickListener(this);
+        btnQueryCount.setOnClickListener(this);
+        btnQueryList.setOnClickListener(this);
+        btnUpload2.setOnClickListener(this);
+
         btnStorageManagerPermission.setOnClickListener(this);
 
 
@@ -109,7 +102,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             }
         });
+
+
         initSDK();
+
+
+        //TODO 受限于Android版本及国内ROM厂商的复杂性，目前没有任何办法可以完全获取话单的主叫号码,
+        // 这里让终端用户自己设置自己的SIM卡号码，如果代码能取到，就用代码取到的，代码取不到，就用设置的。如果不设置，建议禁止外呼！！！
+        CommonDataRepo.setLine1Number("18317893006");
+        CommonDataRepo.setLine2Number("18317893007");
+
     }
 
     //初始化SDK（需要先获取 READ_PHONE_STATE 权限，否则会初始化失败）
@@ -118,19 +120,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //        IpuSoftSDK.init(getApplication(),
 //                Env.OPEN_DEV, new AuthInfo("4571122846924808", "6d8f5dff290bac1dd92708b638046125",
 //                        "gw"));
+//        IpuSoftSDK.init(getApplication(),
+//                Env.OPEN_PRO, new AuthInfo("4615043207659530", "336ce5f4b07e8b656c62d53dba16f1d3",
+//                        "15038779932"));
         IpuSoftSDK.init(getApplication(),
                 Env.OPEN_PRO, new AuthInfo("4615043207659530", "336ce5f4b07e8b656c62d53dba16f1d3",
                         "18317893005"));
+
+
     }
 
 
     @Override
     public void onClick(View v) {
         if (v.getId() == btnDial.getId()) {//拨号
+
             EditText etPhone = (EditText) findViewById(R.id.et_phone);
             String phone = etPhone.getText().toString();
             if (StringUtils.isEmpty(phone)) {
-                ToastUtils.showLoading("请输入号码");
+                // 请输入号码
                 return;
             }
             //TODO 需要先申请 CALL_PHONE READ_PHONE_NUMBERS READ_EXTERNAL_STORAGE READ_CALL_LOG 权限
@@ -141,11 +149,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
              * READ_CALL_LOG 获取通话记录
              */
 
-            //调用系统的拨号组件外呼
-//            PhoneManager.callPhoneBySim(
-//                    phone, DateTimeUtils.getCurrentTime(DateTimePattern.getDateTimeWithSecondFormat()),
-//                    "这里传扩展字段，话单推送时会进行回传", 1);
+            //是否打开通话录音的开关
+            int hasSimRecordPermission = -1;//默认：未知状态（不知道是否打开）
+            if (SysRecordingUtils.isHUAWEI()) {//华为，荣耀
+                hasSimRecordPermission = RxPermissionUtils.checkHuaweiRecord();
+            } else if (SysRecordingUtils.isMIUI()) {//小米，红米
+                hasSimRecordPermission = RxPermissionUtils.checkXiaomiRecord();
+            } else if (SysRecordingUtils.isOPPO()) {//OPPO
+                hasSimRecordPermission = RxPermissionUtils.checkOppoRecord();
+            } else if (SysRecordingUtils.isVIVO()) {//VIVO
+                hasSimRecordPermission = RxPermissionUtils.checkViVoRecord();
+            } else {
+                //TODO 其他机型暂未找到解决方案
+            }
+            XLog.d(TAG + "---hasSimRecordPermission---->" + hasSimRecordPermission);
 
+            if (hasSimRecordPermission == -1) {//未知状态
+
+            } else if (hasSimRecordPermission == 0) {//可能未开启状态
+
+            } else if (hasSimRecordPermission == 1) {//电话录音已经开启
+
+            }
+
+            boolean b = RxPermissionUtils.checkManageStoragePermission();
+            if (!b) {
+                //TODO 未获取文件访问权限，这里可以请提醒客户，务必打开权限，否则无法获取录音
+                try {
+                    try {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                            Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION, Uri.parse("package:" + AppUtils.getAppPackageName()));
+                            AppContext.getActivityContext().startActivity(intent);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                            Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                            AppContext.getActivityContext().startActivity(intent);
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    // ToastUtils.showMessage("请手动打开");
+                }
+            }
 
             SimHttp.getInstance()
                     .callPhoneBySim(phone, new OnSimCallPhoneResultListener<SimRiskControlBean>() {
@@ -237,7 +284,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             //如果话单没有对应的录音文件,file传null
 
             //上传话单（录音）
-            AliYunUploadManager.uploadFile(callLog, file);
+            AliYunUploadManager.uploadRecording(callLog, file);
+        } else if (v.getId() == btnQueryCount.getId()) { //查询上传记录的总条数，用于分页
+
+            List<Integer> list = ArrayUtils.createList(
+                    UploadStatus.UPLOAD_FAILED.getStatus(),
+                    UploadStatus.UPLOAD_SUCCEED.getStatus(),
+                    UploadStatus.UPLOAD_IGNORE.getStatus(),
+                    UploadStatus.UPLOADING.getStatus());
+            RecordingDataRepo.queryRecordCount(list, new RecordingDataRepo.OnRecordCountListener() {
+                @Override
+                public void recordCount(int count) {
+                    Log.d(TAG, "recordCount:-------------->" + count);
+                }
+            });
+
+        } else if (v.getId() == btnQueryList.getId()) {//分页查询上传记录
+
+            List<Integer> list = ArrayUtils.createList(
+                    UploadStatus.UPLOAD_FAILED.getStatus(),
+                    UploadStatus.UPLOAD_SUCCEED.getStatus(),
+                    UploadStatus.UPLOAD_IGNORE.getStatus(),
+                    UploadStatus.UPLOADING.getStatus());
+            RecordingDataRepo.queryRecordList(list, 0, 50, new RecordingDataRepo.OnRecordListListener() {
+                @Override
+                public void recordList(List<SysRecording> list) {
+                    Log.d(TAG, "recordCount:-------------->" + GsonUtils.toJson(list));
+                    if (ArrayUtils.isNotEmpty(list)) {
+                        sysRecording = list.get(0);
+                    }
+                    Log.d(TAG, "recordList: -----------》" + GsonUtils.toJson(sysRecording));
+                }
+            });
+
+        } else if (v.getId() == btnUpload2.getId()) {
+            AliYunUploadManager.uploadRecording(sysRecording, new AliYunUploadManager.OnUploadListener() {
+                @Override
+                public void onSuccess(UploadResponse uploadResponse) {
+
+                }
+
+                @Override
+                public void onError(Throwable e) {
+
+                }
+            });
         }
     }
 }
